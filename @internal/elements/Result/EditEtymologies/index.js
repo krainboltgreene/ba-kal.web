54f10f7848d05e84c4b03b2962d63439d2391968
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2019-06-28 14:46:59 -0700

    Denoting the block it contains

--- @internal/elements/Result/EditEtymologies/index.js
+++ @internal/elements/Result/EditEtymologies/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function EditEtymologies ({etymologies, setEtymologies}) {
-  return <form className="card-body" noValidate onSubmit={(event) => event.preventDefault()}>
+  return <form className="etymologies card-body" noValidate onSubmit={(event) => event.preventDefault()}>
     <section className="form-group">
       <label htmlFor={etymologies}>Etymology</label>
       <textarea id={etymologies} aria-label="the word's etymology" className="form-control" value={etymologies} onChange={(event) => setEtymologies(event.target.value)} />
